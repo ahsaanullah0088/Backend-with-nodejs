b8f30a82fe3a706054c8ff69fa38677f06ae9941
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-06-12 01:34:24 +0500

    ejs done

--- index.js
+++ index.js
@@ -3,17 +3,30 @@ import path from "path";
 
 const app = express()
 
+const users =[];
+
 app.use(express.static(path.join(path.resolve(), "public")));
+app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine" , "ejs");
 // setting up ejs engine
 
-app.get('/' , (req , res) => {
+app.get('/success' , (req , res) => {
 
-    // res.render("index");
-    res.sendFile("index.html");
+    res.render("success");
 });
 
+app.post("/contact" , (req , res) => {
+    console.log(req.body);
+users.push({username : req.body.username , email : req.body.email ,phone : req.body.phone , address : req.body.address});
+    res.redirect("/success");
+});
+
+app.get("/users" , (req , res) => {
+    res.json({
+        users
+    });
+});
 app.listen(5000 , () => {
     console.log('Server is running on port 5000');
 });
