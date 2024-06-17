b5fa5b3b8926129b97bebfad7e9148d6a402915c
Ahsaan Ullah <ahsaanu70@gmail.com>
2024-06-18 01:26:32 +0500

    basic mongodb

--- index.js
+++ index.js
@@ -1,9 +1,22 @@
 import express from "express";
 import path from "path";
+import mongoose from "mongoose";
 
+mongoose.connect("mongodb+srv://ahsaanu70:ahsaanu70@backend.zlc1iox.mongodb.net/?retryWrites=true&w=majority&appName=backend", {
+    dbName: "backend",
+}).then(()=>
+    console.log("Connected to the database")).catch((err) => console.log(err));
+
+const messageSchema = new mongoose.Schema({
+    username: String,
+    email: String,
+    phone: Number,
+    address: String,
+});
+
+const Message = mongoose.model("Message", messageSchema);
 const app = express()
 
-const users =[];
 
 app.use(express.static(path.join(path.resolve(), "public")));
 app.use(express.urlencoded({ extended: true }));
@@ -11,14 +24,19 @@ app.use(express.urlencoded({ extended: true }));
 app.set("view engine" , "ejs");
 // setting up ejs engine
 
+app.get("/" , (req , res) => {
+    res.render("index");
+});
+
 app.get('/success' , (req , res) => {
 
     res.render("success");
-});
+}); 
 
-app.post("/contact" , (req , res) => {
-    console.log(req.body);
-users.push({username : req.body.username , email : req.body.email ,phone : req.body.phone , address : req.body.address});
+app.post("/contact" , async (req , res) => {
+//  const messageData ={username : , email:  ,phone:  , address: };cps
+const {username , email , phone , address} = req.body;
+ await Message.create({username , email  ,phone  , address });
     res.redirect("/success");
 });
 
